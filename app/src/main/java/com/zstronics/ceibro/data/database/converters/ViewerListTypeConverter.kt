617001a4e5d3cd2356dab1913d33ac0595d7e0ff
ibraheemjutt94 <mibrahim@zstronics.com>
2023-01-18 20:03:18 +0500

    Get All subtasks, local DB setup for subtasks and API call to get All subtasks

---
+++ app/src/main/java/com/zstronics/ceibro/data/database/converters/ViewerListTypeConverter.kt
@@ -0,0 +1,22 @@
+package com.zstronics.ceibro.data.database.converters
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.zstronics.ceibro.data.database.models.subtask.AssignedTo
+import com.zstronics.ceibro.data.database.models.subtask.Viewer
+import com.zstronics.ceibro.data.database.models.tasks.ProjectSubTaskStatus
+
+class ViewerListTypeConverter {
+
+    @TypeConverter
+    fun fromString(value: String): List<Viewer> {
+        val type = object : TypeToken<List<Viewer>>() {}.type
+        return Gson().fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<Viewer>): String {
+        return Gson().toJson(list)
+    }
+}
