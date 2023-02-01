9191145107bfdc86aaddbef217c1da0935b77813
tehrimabbas <tehrim.abbas@gomotive.com>
2023-02-01 18:27:48 +0500

    - Fix crash

--- app/src/main/java/com/zstronics/ceibro/data/database/converters/ListConverters.kt
+++ app/src/main/java/com/zstronics/ceibro/data/database/converters/ListConverters.kt
@@ -6,12 +6,12 @@ import com.google.gson.reflect.TypeToken
 
 class ListConverters {
     @TypeConverter
-    fun fromListToString(list: List<String>): String {
+    fun fromListToString(list: List<String>?): String? {
         return Gson().toJson(list)
     }
 
     @TypeConverter
-    fun fromStringToList(string: String): List<String> {
+    fun fromStringToList(string: String?): List<String>? {
         val listType = object : TypeToken<List<String>>() {}.type
         return Gson().fromJson(string, listType)
     }
--- app/src/main/java/com/zstronics/ceibro/data/database/converters/SubTaskCommentsListTypeConverter.kt
+++ app/src/main/java/com/zstronics/ceibro/data/database/converters/SubTaskCommentsListTypeConverter.kt
@@ -9,13 +9,13 @@ import com.zstronics.ceibro.data.database.models.tasks.TaskMember
 class SubTaskCommentsListTypeConverter {
 
     @TypeConverter
-    fun fromString(value: String): List<SubTaskComments> {
+    fun fromString(value: String?): List<SubTaskComments>? {
         val type = object : TypeToken<List<SubTaskComments>>() {}.type
         return Gson().fromJson(value, type)
     }
 
     @TypeConverter
-    fun fromList(list: List<SubTaskComments>): String {
+    fun fromList(list: List<SubTaskComments>?): String? {
         return Gson().toJson(list)
     }
 }
