691dc50967abfcd435de127740d5e1b1738e8b89
tehrimabbas <tehrim.abbas@gomotive.com>
2023-03-12 21:31:29 +0500

    - Create Role Completed. - Edit Role Completed. - Delete Role Completed.

--- app/src/main/java/com/zstronics/ceibro/ui/projects/newproject/group/addnewgroup/CreateGroupRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.zstronics.ceibro.ui.projects.newproject.group.addnewgroup
-
-
-import com.google.gson.annotations.SerializedName
-
-data class CreateGroupRequest(
-    @SerializedName("name")
-    val name: String
-)
