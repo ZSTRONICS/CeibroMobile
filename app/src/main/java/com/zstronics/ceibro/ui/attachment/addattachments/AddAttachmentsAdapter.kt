937f185641e4dceebd34457cdbbaaeec2c1792c0
tehrimabbas <tehrim.abbas@gomotive.com>
2023-03-30 12:47:23 +0500

    - Edit photo from attachment add screen.

--- app/src/main/java/com/zstronics/ceibro/ui/attachment/addattachments/AddAttachmentsAdapter.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/attachment/addattachments/AddAttachmentsAdapter.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.zstronics.ceibro.base.extensions.gone
+import com.zstronics.ceibro.base.extensions.visible
 import com.zstronics.ceibro.databinding.LayoutAddAttachmentBinding
+import com.zstronics.ceibro.ui.attachment.AttachmentTypes
 import com.zstronics.ceibro.ui.attachment.SubtaskAttachment
 import javax.inject.Inject
 
@@ -14,7 +17,8 @@ class AddAttachmentsAdapter @Inject constructor() :
     RecyclerView.Adapter<AddAttachmentsAdapter.AddAttachmentsViewHolder>() {
     var itemClickListener: ((view: View, position: Int, data: SubtaskAttachment?) -> Unit)? =
         null
-
+    var onEditPhoto: ((view: View, position: Int, data: SubtaskAttachment?) -> Unit)? =
+        null
     private var list: MutableList<SubtaskAttachment?> = mutableListOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddAttachmentsViewHolder {
         return AddAttachmentsViewHolder(
@@ -44,6 +48,17 @@ class AddAttachmentsAdapter @Inject constructor() :
     inner class AddAttachmentsViewHolder(private val binding: LayoutAddAttachmentBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: SubtaskAttachment?) {
+            if (item?.attachmentType == AttachmentTypes.Image) {
+                binding.editLayout.visible()
+                binding.editLayout.setOnClickListener {
+                    onEditPhoto?.invoke(it, absoluteAdapterPosition, item)
+                }
+            } else {
+                binding.editLayout.gone()
+            }
+            binding.crossView.setOnClickListener {
+                itemClickListener?.invoke(it, absoluteAdapterPosition, item)
+            }
             binding.crossView.setOnClickListener {
                 itemClickListener?.invoke(it, absoluteAdapterPosition, item)
             }
