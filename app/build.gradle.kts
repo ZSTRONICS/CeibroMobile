e94b0a9b546334279cc48b49c2915e81874b0172
tehrimabbas <tehrim.abbas@gomotive.com>
2023-04-03 02:17:22 +0500

    - One signal integration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,5 @@ dependencies {
     implementation("org.greenrobot:eventbus:3.3.1")
     implementation("com.ericktijerou.koleton:koleton:1.0.0-beta01")
     implementation("com.github.tntkhang:full-screen-image-view-library:1.1.0")
+    implementation("com.onesignal:OneSignal:[4.0.0, 4.99.99]")
 }
--- app/src/main/java/com/zstronics/ceibro/CeibroApplication.kt
+++ app/src/main/java/com/zstronics/ceibro/CeibroApplication.kt
@@ -1,6 +1,7 @@
 package com.zstronics.ceibro
 
 import android.app.Application
+import com.onesignal.OneSignal
 import com.zstronics.ceibro.data.base.interceptor.SessionValidator
 import com.zstronics.ceibro.data.repos.auth.IAuthRepository
 import com.zstronics.ceibro.data.sessions.SessionManager
@@ -17,15 +18,26 @@ open class CeibroApplication : Application() {
 
     @Inject
     lateinit var authApi: IAuthRepository
+
     override fun onCreate() {
         super.onCreate()
         ceibroApplication = this
         sessionValidator.setAuthRepository(authApi)
         sessionValidator.setSessionManager(sessionManager)
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
     }
 
     companion object {
         var ceibroApplication: CeibroApplication? = null
             private set
+
+        const val ONESIGNAL_APP_ID = "f9cac0cf-3e04-486c-b87f-f84dc2c8e517"
+//        const val ONESIGNAL_APP_ID = "7ac7a441-9500-4f47-b370-c523db13de03"
     }
 }
