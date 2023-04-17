cc5da0405ea6295a263ac83fb12e59b1045e2589
Muhammad Ibraheem <115614025+ibraheemjutt94@users.noreply.github.com>
2023-04-17 22:48:53 +0500

    - app live version for production release (#72)

--- buildSrc/src/main/kotlin/com/zstronics/dependencies/AppConfig.kt
+++ buildSrc/src/main/kotlin/com/zstronics/dependencies/AppConfig.kt
@@ -4,11 +4,11 @@ object AppConfig {
     const val COMPILE_SDK_VERSION = 33
     const val TARGET_SDK_VERSION = COMPILE_SDK_VERSION
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.1.0"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "2.0.0"
 
-    const val VERSION_CODE_LIVE = 1
-    const val VERSION_NAME_LIVE = "0.0.1"
+    const val VERSION_CODE_LIVE = 2
+    const val VERSION_NAME_LIVE = "1.0.0"
 
     const val APP_ID = "com.zstronics.ceibro"
     const val APP_ID_LIVE = "com.zstronics.ceibro"
