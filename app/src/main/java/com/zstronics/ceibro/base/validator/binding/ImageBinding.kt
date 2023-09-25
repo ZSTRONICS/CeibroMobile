7818462bba0e828006c73835a6b2ef96effc8499
tayyab <mtayyab@zstronics.com>
2023-09-25 20:32:23 +0500

    Implementation of Progress bar on image Loading.

--- app/src/main/java/com/zstronics/ceibro/base/validator/binding/ImageBinding.kt
+++ app/src/main/java/com/zstronics/ceibro/base/validator/binding/ImageBinding.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.zstronics.ceibro.R
 
 
@@ -22,10 +24,52 @@ object ImageBinding {
         imageView: AppCompatImageView,
         imageUrl: String?,
     ) {
-        Glide.with(imageView.context)
+        val context = imageView.context
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
+        val requestOptions = RequestOptions()
+            .placeholder(circularProgressDrawable)
+            .error(R.drawable.profile_img)
+            .skipMemoryCache(true)
+            .centerCrop()
+
+        Glide.with(context)
             .load(imageUrl)
-            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-            .placeholder(R.drawable.profile_img)
+            .apply(requestOptions)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(imageView)
+    }
+
+    @JvmStatic
+    @BindingAdapter(
+        value = ["app:loadSmallImageFromUrl"],
+        requireAll = false
+    )
+    fun loadSmallProductImage(
+        imageView: AppCompatImageView,
+        imageUrl: String?,
+    ) {
+        val context = imageView.context
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 4f
+        circularProgressDrawable.centerRadius = 14f
+        circularProgressDrawable.start()
+
+        val requestOptions = RequestOptions()
+            .placeholder(circularProgressDrawable)
+            .error(R.drawable.profile_img)
+            .skipMemoryCache(true)
+            .centerCrop()
+
+        Glide.with(context)
+            .load(imageUrl)
+            .apply(requestOptions)
+            .transition(DrawableTransitionOptions.withCrossFade())
             .into(imageView)
     }
 
