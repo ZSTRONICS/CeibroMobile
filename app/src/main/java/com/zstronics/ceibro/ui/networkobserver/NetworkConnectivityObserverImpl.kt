4081d39fee29c35a8e8bddd999d8502db5712afd
syedtehrimabbas <syedtehrimabbas@gmail.com>
2023-09-26 13:56:25 +0500

    Update NetworkConnectivityObserverImpl.kt
    - Do not show online on first call.

--- app/src/main/java/com/zstronics/ceibro/ui/networkobserver/NetworkConnectivityObserverImpl.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/networkobserver/NetworkConnectivityObserverImpl.kt
@@ -2,17 +2,15 @@ package com.zstronics.ceibro.ui.networkobserver
 
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkCapabilities
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
 
 internal class NetworkConnectivityObserverImpl(
     connectivityManager: ConnectivityManager
 ) : NetworkConnectivityObserver {
-
+    var appFirstRun = true
     private val networkStatus = MutableSharedFlow<NetworkConnectivityObserver.Status>(
         replay = 1,
         extraBufferCapacity = 1,
@@ -24,7 +22,9 @@ internal class NetworkConnectivityObserverImpl(
             ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
-                networkStatus.tryEmit(NetworkConnectivityObserver.Status.Available)
+                if (appFirstRun.not())
+                    networkStatus.tryEmit(NetworkConnectivityObserver.Status.Available)
+                appFirstRun = false
             }
 
             override fun onLosing(network: Network, maxMsToLive: Int) {
@@ -42,14 +42,6 @@ internal class NetworkConnectivityObserverImpl(
                 networkStatus.tryEmit(NetworkConnectivityObserver.Status.Unavailable)
             }
         })
-
-        val activeNetwork = connectivityManager.activeNetwork
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
-        if (networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
-            networkStatus.tryEmit(NetworkConnectivityObserver.Status.Available)
-        } else {
-            networkStatus.tryEmit(NetworkConnectivityObserver.Status.Unavailable)
-        }
     }
 
     override fun observe(): Flow<NetworkConnectivityObserver.Status> =
