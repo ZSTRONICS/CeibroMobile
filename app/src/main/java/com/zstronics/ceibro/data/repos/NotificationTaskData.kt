6a4ae68923105831959d71d89bb7725424134bda
tayyab <mtayyab@zstronics.com>
2023-10-25 22:29:42 +0500

    Bug Fixes : -Working on the navigation through the pending intents from notifications. -Custom notifications for both expand and collapsed state.

---
+++ app/src/main/java/com/zstronics/ceibro/data/repos/NotificationTaskData.kt
@@ -0,0 +1,9 @@
+package com.zstronics.ceibro.data.repos
+
+data class NotificationTaskData(
+    val avatar: String,
+    val creator: String,
+    val description: String,
+    val taskId: String,
+    val topic: String
+)
