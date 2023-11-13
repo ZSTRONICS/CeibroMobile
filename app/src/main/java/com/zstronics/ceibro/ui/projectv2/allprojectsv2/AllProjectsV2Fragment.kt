56329e4591fdb99058c805bbcabd90b9550a4d01
tayyab <mtayyab@zstronics.com>
2023-11-13 20:42:33 +0500

    Working on Location Module UIs. -Minor UI fixes.

--- app/src/main/java/com/zstronics/ceibro/ui/projectv2/allprojectsv2/AllProjectsV2Fragment.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/projectv2/allprojectsv2/AllProjectsV2Fragment.kt
@@ -1,12 +1,16 @@
 package com.zstronics.ceibro.ui.projectv2.allprojectsv2
 
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import com.zstronics.ceibro.BR
 import com.zstronics.ceibro.R
@@ -106,7 +110,7 @@ class AllProjectsV2Fragment(callback: (Int) -> Unit) :
                 }
 
                 2 -> {
-                    showDialog()
+                    showHideTaskDialog(requireContext())
                 }
             }
 
@@ -114,21 +118,27 @@ class AllProjectsV2Fragment(callback: (Int) -> Unit) :
 
     }
 
-    private fun showDialog() {
-        val dialog = Dialog(requireContext())
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.layout_task_hide_dialog)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        val tvYes: TextView = dialog.findViewById(R.id.tvYes)
-        val tvNo: TextView = dialog.findViewById(R.id.tvNo)
 
-        tvYes.setOnClickListener {
-            dialog.dismiss()
+    private fun showHideTaskDialog(context: Context) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.layout_custom_dialog, null)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context).setView(view)
+        val alertDialog = builder.create()
+
+        val yesBtn = view.findViewById<Button>(R.id.yesBtn)
+        val noBtn = view.findViewById<Button>(R.id.noBtn)
+        val dialogText = view.findViewById<TextView>(R.id.dialog_text)
+        dialogText.text = context.resources.getString(R.string.do_you_want_to_un_hide_this_project)
+        alertDialog.window?.setBackgroundDrawable(null)
+        alertDialog.show()
+
+        yesBtn.setOnClickListener {
+            alertDialog.dismiss()
         }
-        tvNo.setOnClickListener {
-            dialog.dismiss()
+
+        noBtn.setOnClickListener {
+            alertDialog.dismiss()
         }
-        dialog.show()
     }
 }
--- app/src/main/java/com/zstronics/ceibro/ui/projectv2/hiddenprojectv2/HiddenProjectsV2Fragment.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/projectv2/hiddenprojectv2/HiddenProjectsV2Fragment.kt
@@ -1,16 +1,21 @@
 package com.zstronics.ceibro.ui.projectv2.hiddenprojectv2
 
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import com.zstronics.ceibro.BR
 import com.zstronics.ceibro.R
 import com.zstronics.ceibro.base.navgraph.BaseNavViewModelFragment
+import com.zstronics.ceibro.data.database.models.tasks.CeibroTaskV2
 import com.zstronics.ceibro.data.repos.chat.room.Project
 import com.zstronics.ceibro.databinding.FragmentHiddenProjectsV2Binding
 import com.zstronics.ceibro.ui.projectv2.allprojectsv2.AllProjectAdapter
@@ -108,7 +113,7 @@ class HiddenProjectsV2Fragment(callback: (Int) -> Unit) :
                 }
 
                 2 -> {
-                    showDialog()
+                    showHideTaskDialog(requireContext())
                 }
             }
 
@@ -116,21 +121,26 @@ class HiddenProjectsV2Fragment(callback: (Int) -> Unit) :
 
     }
 
-    private fun showDialog() {
-        val dialog = Dialog(requireContext())
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.layout_task_hide_dialog)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        val tvYes: TextView = dialog.findViewById(R.id.tvYes)
-        val tvNo: TextView = dialog.findViewById(R.id.tvNo)
+    private fun showHideTaskDialog(context: Context) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.layout_custom_dialog, null)
 
-        tvYes.setOnClickListener {
-            dialog.dismiss()
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context).setView(view)
+        val alertDialog = builder.create()
+
+        val yesBtn = view.findViewById<Button>(R.id.yesBtn)
+        val noBtn = view.findViewById<Button>(R.id.noBtn)
+        val dialogText = view.findViewById<TextView>(R.id.dialog_text)
+        dialogText.text = context.resources.getString(R.string.do_you_want_to_un_hide_this_project)
+        alertDialog.window?.setBackgroundDrawable(null)
+        alertDialog.show()
+
+        yesBtn.setOnClickListener {
+            alertDialog.dismiss()
         }
-        tvNo.setOnClickListener {
-            dialog.dismiss()
+
+        noBtn.setOnClickListener {
+            alertDialog.dismiss()
         }
-        dialog.show()
     }
 }
