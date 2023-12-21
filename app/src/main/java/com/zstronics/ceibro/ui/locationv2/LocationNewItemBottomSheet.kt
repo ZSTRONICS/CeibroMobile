a7d86255e81a38b664a77ba04217ea896d3c56ed
ibraheemjutt94 <mibrahim@zstronics.com>
2023-12-21 18:34:32 +0500

    - location long press point - bottom sheet for new items

---
+++ app/src/main/java/com/zstronics/ceibro/ui/locationv2/LocationNewItemBottomSheet.kt
@@ -0,0 +1,42 @@
+package com.zstronics.ceibro.ui.locationv2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.zstronics.ceibro.R
+import com.zstronics.ceibro.databinding.LayoutLocationNewItemBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LocationNewItemBottomSheet() : BottomSheetDialogFragment() {
+    lateinit var binding: LayoutLocationNewItemBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.layout_location_new_item,
+            container,
+            false
+        )
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.closeBtn.setOnClickListener {
+            dismiss()
+        }
+    }
+
+}
