f0bb3185be4e065e235f134a0bc6164a156e16a9
tayyab <mtayyab@zstronics.com>
2024-02-29 19:50:39 +0500

    worked on task type bottom sheet.

--- app/src/main/java/com/zstronics/ceibro/ui/tasks/v3/fragments/ongoing/OngoingFragment.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/tasks/v3/fragments/ongoing/OngoingFragment.kt
@@ -1,9 +1,13 @@
 package com.zstronics.ceibro.ui.tasks.v3.fragments.ongoing
 
+import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.zstronics.ceibro.BR
 import com.zstronics.ceibro.R
+import com.zstronics.ceibro.base.extensions.shortToastNow
 import com.zstronics.ceibro.base.navgraph.BaseNavViewModelFragment
 import com.zstronics.ceibro.databinding.FragmentOngoingBinding
 import com.zstronics.ceibro.ui.inbox.adapter.InboxAdapter
@@ -14,8 +18,10 @@ import javax.inject.Inject
 class OngoingFragment :
     BaseNavViewModelFragment<FragmentOngoingBinding, IOngoing.State, OngoingVM>() {
 
+
     @Inject
     lateinit var adapter: InboxAdapter
+    private var fragmentManager: FragmentManager? = null
     override val bindingVariableId = BR.viewModel
     override val bindingViewStateVariableId = BR.viewState
     override val viewModel: OngoingVM by viewModels()
@@ -25,7 +31,12 @@ class OngoingFragment :
         when (id) {
 
             R.id.clTaskType -> {
+                fragmentManager?.let {
+                    chooseTaskType(it) { type ->
+                        mViewDataBinding.tvtaskType.text= type
 
+                    }
+                }
             }
 
             R.id.imgSearch -> {
@@ -40,9 +51,23 @@ class OngoingFragment :
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentManager = childFragmentManager
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         mViewDataBinding.taskRV.adapter = adapter
 
+    }
+
+    private fun chooseTaskType(fragmentManager: FragmentManager, callback: (String) -> Unit) {
+        val sheet = TaskTypeBottomSheet {
+            callback.invoke(it)
+        }
+
+        sheet.isCancelable = true
+        sheet.show(fragmentManager, "TaskTypeBottomSheet")
     }
 }
