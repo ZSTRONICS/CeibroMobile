366bc5a4a1b6193983b6ba5d0f193e462870cca6
ibraheemjutt94 <mibrahim@zstronics.com>
2024-03-06 18:08:37 +0500

    - task structural change with V3 States and Adapters

--- app/src/main/java/com/zstronics/ceibro/ui/tasks/v3/TasksParentTabV3Fragment.kt
+++ app/src/main/java/com/zstronics/ceibro/ui/tasks/v3/TasksParentTabV3Fragment.kt
@@ -59,8 +59,8 @@ class TasksParentTabV3Fragment :
             }
 
             R.id.taskType -> {
-                chooseTaskType(viewModel.selectedTaskType) { type ->
-                    viewModel.selectedTaskType = type
+                chooseTaskType(viewModel.selectedTaskTypeState) { type ->
+                    viewModel.selectedTaskTypeState = type
                     var typeToShow = ""
                     if (type.equals(TaskRootStateTags.All.tagValue, true)) {
                         typeToShow = "All"
