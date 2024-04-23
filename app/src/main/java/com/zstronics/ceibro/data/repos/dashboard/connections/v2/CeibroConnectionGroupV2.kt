530d97ffd554308f6ff3b2ea51dec1b78175717c
ibraheemjutt94 <mibrahim@zstronics.com>
2024-04-23 13:21:12 +0500

    - group structure update

--- app/src/main/java/com/zstronics/ceibro/data/repos/dashboard/connections/v2/CeibroConnectionGroupV2.kt
+++ app/src/main/java/com/zstronics/ceibro/data/repos/dashboard/connections/v2/CeibroConnectionGroupV2.kt
@@ -6,6 +6,8 @@ import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
 import com.zstronics.ceibro.data.base.BaseResponse
 import com.zstronics.ceibro.data.database.TableNamesV2
+import com.zstronics.ceibro.data.database.models.tasks.AssignedToState
+import com.zstronics.ceibro.data.database.models.tasks.TaskMemberDetail
 import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = TableNamesV2.ConnectionGroup, primaryKeys = ["_id"])
@@ -14,14 +16,26 @@ import kotlinx.parcelize.Parcelize
 data class CeibroConnectionGroupV2(
     @SerializedName("_id")
     val _id: String,
-    @SerializedName("contacts")
-    val contacts: List<GroupContact>,
-    @SerializedName("createdAt")
-    val createdAt: String,
-    @SerializedName("creator")
-    val creator: String,
     @SerializedName("name")
     val name: String,
+    @SerializedName("creator")
+    val creator: TaskMemberDetail,
+    @SerializedName("confirmer")
+    val confirmer: List<TaskMemberDetail>,
+    @SerializedName("viewer")
+    val viewer: List<TaskMemberDetail>,
+    @SerializedName("assignedToState")
+    val assignedToState: List<AssignedToState>,
+    @SerializedName("groupAdmins")
+    val groupAdmins: List<TaskMemberDetail>,
+    @SerializedName("sharedWith")
+    val sharedWith: List<TaskMemberDetail>,
+    @SerializedName("access")
+    val access: List<String>,
+    @SerializedName("isPublic")
+    val isPublic: Boolean,
+    @SerializedName("createdAt")
+    val createdAt: String,
     @SerializedName("updatedAt")
     val updatedAt: String
 ) : BaseResponse(), Parcelable
